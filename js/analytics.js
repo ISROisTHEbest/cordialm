dfcfaea22d785ee8cd4dbe6e8664d32d361a7e50
ISROisTHEbest <taksheelshah2009@gmail.com>
2026-01-17 16:36:27 +0530

    V1.2.3 Added Analytics Updated Page Titles

---
+++ js/analytics.js
@@ -0,0 +1,10 @@
+const script = document.createElement('script');
+script.async = true;
+script.src = "https://www.googletagmanager.com/gtag/js?id=G-BE5T0XD43B"
+document.head.appendChild(script);
+
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-BE5T0XD43B');
